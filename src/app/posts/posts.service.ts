87ff9cb975700c2956aca2f543704c7be7d4eda1
Ryan Transfiguracion <rtransfig@gmail.com>
2018-09-19 02:12:42 -0700

    Added Mongoose package.

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,9 +1,9 @@
-import { Post } from './post.model';
-import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { Post } from "./post.model";
+import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
+import { HttpClient } from "@angular/common/http";
 
-@Injectable({ providedIn: 'root' })
+@Injectable({ providedIn: "root" })
 export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
@@ -15,7 +15,7 @@ export class PostsService {
       .get<{ message: string; posts: Post[] }>(
         `http://localhost:3000/api/posts`
       )
-      .subscribe((postData) => {
+      .subscribe(postData => {
         this.posts = postData.posts;
         this.postsUpdated.next([...this.posts]);
       });
@@ -31,7 +31,13 @@ export class PostsService {
       title: title,
       content: content
     };
-    this.posts.push(post);
-    this.postsUpdated.next([...this.posts]);
+    this.http
+      .post<{ message: string }>("http://localhost:3000/api/posts", post)
+      .subscribe(response => {
+        console.log(response.message);
+        // Pessimistic updating
+        this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 }
