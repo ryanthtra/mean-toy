9812ba5c5cba5c28b208ee6fb5d86adc125932ee
Ryan Transfiguracion <rtransfig@gmail.com>
2018-09-20 05:36:06 -0700

    Added backend adding post to db.

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+const Post = require('./models/post');
 const app = express();
+
+mongoose
+  .connect('mongodb://ryanthtra:Qwer1234@ds161062.mlab.com:61062/mean-db-toy')
+  .then(() => {
+    console.log('Connected to database');
+  })
+  .catch(() => {
+    console.log('Connection failed!!!');
+  });
 
 app.use(bodyParser.json());
 
@@ -19,8 +30,11 @@ app.use((req, res, next) => {
 });
 
 app.post('/api/posts', (req, res, next) => {
-  const post = req.body;
-  console.log(post);
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save();
   res.status(201).json({
     message: 'Post added successfully!'
   });
